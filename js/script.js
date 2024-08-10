8084d3c001641cc0d185be502d4d6073c5e377e0
Dimash <example@gmail.com>
2024-08-10 18:16:11 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// alert ('Hello');
+
+// console.log('1');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you 18?', '18');
+// console.log((answer + 10));
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(answers);
+// console.log(typeof(answers));
+// document.write(answers);
+
+// console.log(answer);
+// console.log(1);
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = { 
+//     a:50
+// };
+
+// obj.a = 20;
+// console.log(obj);
+// console.log(name);
+// var name = 'Ivan'
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// const category = 'toys';
+// const danu = 'balls';
+// console.log(`https://someurl.com/${category}/5/${danu}`);
+
+// const user = 'Ivan';
+// alert(`Hi, ${user}`);
+
+// console.log(4 + +'number');
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log(4*2 == 8);
+// console.log(4*2 === '8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(!isChecked || isClose);
---
+++ js/variables.js
@@ -0,0 +1,47 @@
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ", Длина: " + vehicleBodyLength);
+
+
+// let und;
+// console.log(und)
+
+// const obj = {
+//     name: 'Dimash',
+//     age: 23,
+//     isMarried: false
+// };
+// console.log(obj.age);
+
+// let arr = ['orange.png', 'apple.jpg', 5, [], {}];
+// console.log(arr[4]);
+
+// const arrObj = { a: 'a', 1: 'c', '3': 'd' };
+
+// const h = 'h';
+
+// arrObj[h] = 'adamdar' 
+// arrObj.b = '1234';
+// arrObj['f'] = '4567';
+// console.log(arrObj['b']);
+// console.log(arrObj['f']);
+
+// console.log(arrObj.h);
+
+// const storeDescription = {
+//     abc:{
+//         def:{
+
+//         }
+//     }
+// }
+
+const storeName = 'Bereke';
+const storeDescription = {
+    budget: 10000,
+    employees: ['Dimash', 'Madi', 'Alibi'],
+    products: {ball: 5000, bag: 3000},
+    open: true
+};
+console.log(storeDescription);
